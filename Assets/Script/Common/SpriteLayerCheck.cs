a461c72f83d1e9ef971db8e75ec14a2d4b8a45e3
FSISF <a1455785@gmail.com>
2017-06-04 12:25:25 +0800

    一般地圖角色圖層動態切換製作

---
+++ Assets/Script/Common/SpriteLayerCheck.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteLayerCheck : MonoBehaviour
+{
+    [SerializeField]
+    private Transform ThisTransform = null;
+
+    [SerializeField]
+    private SpriteRenderer ThisSpriteRenderer = null;
+
+    void Start()
+    {
+        ThisSpriteRenderer.sortingOrder = Mathf.RoundToInt(ThisTransform.position.y * 100) * -1;
+    }
+
+    void Update()
+    {
+        if (!this.gameObject.isStatic)
+        {
+            ThisSpriteRenderer.sortingOrder = Mathf.RoundToInt(ThisTransform.position.y * 100) * -1;
+        }
+    }
+}
